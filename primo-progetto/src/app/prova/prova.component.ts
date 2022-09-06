53d5ff8d82e758b5fc76d18a96986b738ef36e4d
AD25 <angelo251158@gmail.com>
2022-09-06 11:29:55 +0200

    Data binding

--- primo-progetto/src/app/prova/prova.component.ts
+++ primo-progetto/src/app/prova/prova.component.ts
@@ -7,10 +7,39 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
 })
 export class ProvaComponent implements OnInit, AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, DoCheck, OnDestroy {
 
+  //STRING INTERPOLATION
   nomecane = "roger"
+  cani = [
+    {nome: 'roger',
+    razza: 'golden',
+    descrizione: 'cane normale'}
+  ]
+
+  //PROPERTY BINDING
+  isDisabled = true
+  immagine = ''
+  immagine1 = 'https://assets2.razerzone.com/images/pnx.assets/618c0b65424070a1017a7168ea1b6337/razer-wallpapers-page-hero-mobile.jpg'
+  immagine2 = 'https://material.angular.io/assets/img/examples/shiba2.jpg'
 
   constructor() {
     console.log("costruttore")
+  }
+
+  ngOnInit(): void {
+    //PROPERTY BINDING
+    console.log("ngOnInit")
+    setInterval(()=>{
+      this.isDisabled = !this.isDisabled
+    }, 2000)
+    let counter = 0
+    setInterval(()=>{
+      if(counter%2==0){
+        this.immagine = this.immagine1
+      } else {
+        this.immagine = this.immagine2
+      }
+      counter++
+    }, 2000)
   }
   ngAfterContentChecked(): void {
     console.log("ngAfterContentChecked")
@@ -30,9 +59,4 @@ export class ProvaComponent implements OnInit, AfterContentChecked, AfterContent
   ngOnDestroy(): void {
     console.log("ngOnDestroy")
   }
-
-  ngOnInit(): void {
-    console.log("ngOnInit")
-  }
-
 }
