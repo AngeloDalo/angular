67d752b80c1ed1b5dd2fde0cb090b82b06da4b83
AD25 <angelo251158@gmail.com>
2022-09-17 11:07:25 +0200

    Variabili template

--- primo-progetto/src/app/app.component.ts
+++ primo-progetto/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'prova-angular';
   isVisible = true;
   numero = 3;
@@ -16,8 +16,18 @@ export class AppComponent {
     {nome: "marco", cognome: "verde", isOnline: false},
   ]
 
+  //VARIABILE TEMPLATE
+  //inputSaluti si chiama valoreInput
+  @ViewChild('inputSaluti') valoreInput!: ElementRef<HTMLInputElement>
+  ngAfterViewInit(): void {
+    console.log(this.valoreInput.nativeElement.value)
+  }
+  onClick3(){
+    console.log(this.valoreInput.nativeElement.value)
+  }
+
   //Se avessi messo e: any potevo evitare HTMLInputElement, ho fatto un casting
-  onInput(e: Event){
+  onInput(e: Event): void{
     this.title = ((<HTMLInputElement>e.target).value);
   }
 
