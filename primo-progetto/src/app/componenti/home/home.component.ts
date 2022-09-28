d1714beeab3124b31b3e04d7d7b9c5d61843a166
AD25 <angelo251158@gmail.com>
2022-09-28 10:17:27 +0200

    form

--- primo-progetto/src/app/componenti/home/home.component.ts
+++ primo-progetto/src/app/componenti/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,13 +8,27 @@ import { NgForm } from '@angular/forms';
 })
 export class HomeComponent implements OnInit {
 
+  //REACTIVE FORM
+  homeform!: FormGroup;
+
+
   constructor() { }
 
   ngOnInit(): void {
+    this.homeform = new FormGroup({
+      nome: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      colore: new FormControl()
+    })
   }
 
-  onSubmit(form: NgForm) {
-    console.log(form)
+  //REACTIVE FORM
+  onSubmit() {
+    console.log(this.homeform)
   }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form)
+  // }
 
 }
